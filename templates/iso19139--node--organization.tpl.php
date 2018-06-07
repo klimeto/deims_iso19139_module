5410c7ffa9c53fa7d6446f29e23a258a11ed9ef3
stopopol <christoph.wohner@umweltbundesamt.at>
2018-06-07 15:12:16 +0200

    Update iso19139--node--organization.tpl.php

--- templates/iso19139--node--organization.tpl.php
+++ templates/iso19139--node--organization.tpl.php
@@ -1,1 +1,10 @@
-﻿ <gmd:organisationName><gco:CharacterString><?php print $label; ?></gco:CharacterString></gmd:organisationName>
+<?php
+/**
+ * Needs to be extended in order to incorporate the organisation url
+ */
+?>
+<gmd:CI_ResponsibleParty>
+	<gmd:organisationName>
+		<gco:CharacterString><?php print $label; ?></gco:CharacterString>
+	</gmd:organisationName>
+</gmd:CI_ResponsibleParty>
